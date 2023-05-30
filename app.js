841afe2ddbcfd055e65d9aa16434e9ad5e171c14
Rohit <singh.rohitraj122@gmail.com>
2023-05-30 13:54:39 +0530

    api complete

--- app.js
+++ app.js
@@ -8,9 +8,16 @@ const productRouter=require('./routes/products')
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+
 app.use('/api/v1/products',productRouter)
 app.use(errorHandler);
 app.use(notFound);
+
 
 
 
